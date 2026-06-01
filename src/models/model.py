2b38d43ebdb1529ebba6f4517fdd2b6472377db3
Elliot Hong <ehongia05@gmail.com>
2026-06-01 12:32:52 -0400

    define cnn for cifar-10 in models.py

---
+++ src/models/model.py
@@ -0,0 +1,22 @@
+#import libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    #temp lightweight cnn for CIFAR-10
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1) #make flat
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
